0ab5e60d6ecff418361f23251668b37bb7ed6218
raw98 <78865860+raw98@users.noreply.github.com>
2021-08-01 03:05:37 +0200

    rawand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component'
+import  {MatSidenavModule} from '@angular/material/sidenav'
+import  {MatIconModule} from '@angular/material/icon'
 
+const routes : Routes=[
+  {path: '' , component:Page1Component},
+  {path: 'page1' , component:Page1Component},
+  {path: 'page2' , component:Page2Component},
+  {path: 'navbar' , component:NavbarComponent},
+  {path: '404' , component:NotfoundComponent},
+  { path : '**' , redirectTo : '404' }
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Page1Component,
+    Page2Component,
+    NotfoundComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
