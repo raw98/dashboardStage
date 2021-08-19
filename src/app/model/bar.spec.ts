4f6b996ef9c5e34808405dad70ab09250787626d
eyaMtss <eya.mattoussii@gmail.com>
2021-08-19 21:41:09 +0100

    add bar model

---
+++ src/app/model/bar.spec.ts
@@ -0,0 +1,7 @@
+import { Bar } from './bar';
+
+describe('Bar', () => {
+  it('should create an instance', () => {
+    expect(new Bar()).toBeTruthy();
+  });
+});
---
+++ src/app/model/bar.ts
@@ -0,0 +1,4 @@
+export class Bar {
+    mois !: string;
+    chiffre !: number;
+}
