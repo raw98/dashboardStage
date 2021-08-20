665edd90d13cee313baca79ae2bd4a70f3981333
eyaMtss <eya.mattoussii@gmail.com>
2021-08-20 17:11:36 +0100

    add bar  chart class

---
+++ src/app/models/bar-chart.spec.ts
@@ -0,0 +1,7 @@
+import { BarChart } from './bar-chart';
+
+describe('BarChart', () => {
+  it('should create an instance', () => {
+    expect(new BarChart()).toBeTruthy();
+  });
+});
---
+++ src/app/models/bar-chart.ts
@@ -0,0 +1,4 @@
+export class BarChart {
+    mois !: string;
+    chiffre !: number;
+}
