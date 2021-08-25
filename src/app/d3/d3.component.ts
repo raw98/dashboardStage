a5ab29c4cd9906def210aed77a94d8093092fdb7
raw98 <78865860+raw98@users.noreply.github.com>
2021-08-25 22:50:43 +0200

    rawand

--- src/app/d3/d3.component.ts
+++ src/app/d3/d3.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Attribute, Component, HostListener, Input, OnInit } from '@angular/core';
 import * as d3 from "d3";
 declare const d33: any;
 @Component({
@@ -8,7 +8,18 @@ declare const d33: any;
 })
 export class D3Component implements OnInit {
 @Input() isOpen:boolean=false;
-  constructor() { }
+public innerWidth:number;
+
+  constructor() { 
+    this.innerWidth = window.innerWidth;
+
+  }
+  @HostListener('window:resize', ['$event'])
+onResize(event:any) {
+  this.innerWidth = event.target.innerWidth ;
+  console.log(this.innerWidth);
+  
+}
 
   ngOnInit(): void {
     d33();
