62a095fec72d546552e793a07a16642dcf6db05b
eyaMtss <eya.mattoussii@gmail.com>
2021-08-25 17:28:38 +0100

    add chart2 class

---
+++ src/app/models/chart2.spec.ts
@@ -0,0 +1,7 @@
+import { Chart2 } from './chart2';
+
+describe('Chart2', () => {
+  it('should create an instance', () => {
+    expect(new Chart2()).toBeTruthy();
+  });
+});
---
+++ src/app/models/chart2.ts
@@ -0,0 +1,6 @@
+export class Chart2 {
+    prod !: string;
+    jour !: string;
+    vente !: number;
+    continent !:string;
+}
