b6578ee8329012aa38f11983213badb4a51956ba
raw98 <78865860+raw98@users.noreply.github.com>
2021-08-27 01:16:15 +0200

    rawand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  
+ 
   title = 'dashboardStage';
   test:boolean=true;
-  
+  public innerWidth:number;
+  constructor() { 
+    this.innerWidth = window.innerWidth;
+  }
+  ngOnInit(): void {
+    this.initializeSide();
+}
   changeModeNav(value:string){
     this.test=!this.test;
+  }
+  initializeSide(){
+    if(this.innerWidth<=767)
+    this.changeModeNav("ok");
   }
 }
